2ee6a7f83cd9b922b745bb37d58fdc28bdaae818
eboreg99 <134841205+eboreg99@users.noreply.github.com>
2025-05-19 17:07:50 +0200

    Update battery-bar-card.js

--- battery-bar-card.js
+++ battery-bar-card.js
@@ -84,7 +84,7 @@ class BatteryBarCard extends HTMLElement {
         display: flex;
         align-items: center;
         justify-content: space-between;
-        padding: 8px 16px;
+        padding: 8px;
       }
       .card {
         padding: 16px;
